9d1e35a03338e2e61f63c9930817192ce2124eb6
dafex301 <lfahrel@gmail.com>
2021-10-17 15:23:32 +0700

    alert message and dynamic title

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var methodOverride = require('method-override');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
 
 // import mongoose
 var mongoose = require('mongoose');
@@ -22,6 +24,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(methodOverride('_method'));
+app.use(
+	session({
+		secret: 'keyboard cat',
+		resave: false,
+		saveUninitialized: true,
+		cookie: { maxAge: 60000 },
+	})
+);
+app.use(flash());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
