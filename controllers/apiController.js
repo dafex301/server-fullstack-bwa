5676bb00c66303fabf56da5fab648f3b5b1a3197
dafex301 <lfahrel@gmail.com>
2021-10-20 15:37:44 +0700

    api detail page

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -2,6 +2,7 @@ const Item = require('../models/Item');
 const Treasure = require('../models/Activity');
 const Traveler = require('../models/Booking');
 const Category = require('../models/Category');
+const Bank = require('../models/Bank');
 
 module.exports = {
 	landingPage: async (req, res) => {
@@ -41,6 +42,16 @@ module.exports = {
 					}
 				}
 			}
+			const testimonial = {
+				_id: 'asd1293uasdads1',
+				imageUrl: '/images/testimonial1.jpg',
+				name: 'Happy Family',
+				rate: 4.55,
+				content:
+					'What a great trip with my family and I should try again next time soon ...',
+				familyName: 'Fahrel',
+				familyOccupation: 'Tech Enthusiast',
+			};
 
 			res.status(200).json({
 				hero: {
@@ -50,6 +61,7 @@ module.exports = {
 				},
 				mostPicked,
 				category,
+				testimonial,
 			});
 		} catch (error) {
 			console.log(error);
@@ -57,5 +69,40 @@ module.exports = {
 				message: 'Internal server error',
 			});
 		}
+	},
+
+	detailPage: async (req, res) => {
+		try {
+			const { id } = req.params;
+			const item = await Item.findOne({ _id: id })
+				.populate({
+					path: 'featureId',
+					select: '_id name qty imageUrl',
+				})
+				.populate({
+					path: 'activityId',
+					select: '_id name type imageUrl',
+				})
+				.populate({ path: 'imageId', select: '_id imageUrl' });
+
+			const bank = await Bank.find();
+
+			const testimonial = {
+				_id: 'asd1293uasdads1',
+				imageUrl: '/images/testimonial2.jpg',
+				name: 'Happy Family',
+				rate: 4.55,
+				content:
+					'What a great trip with my family and I should try again next time soon ...',
+				familyName: 'Fahrel',
+				familyOccupation: 'Tech Enthusiast',
+			};
+
+			res.status(200).json({
+				...item._doc,
+				bank,
+				testimonial,
+			});
+		} catch (error) {}
 	},
 };
